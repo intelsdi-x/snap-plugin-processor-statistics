9d68632b83f4dd40a4f998f996f83a12eb1a1c6e
Balaji Subramaniam <balaji.warft@gmail.com>
2016-05-16 21:42:50 +0000

    Added unit tests.

---
+++ statistics/statistics_test.go
@@ -0,0 +1,148 @@
+//
+// +build unit
+
+/*
+http://www.apache.org/licenses/LICENSE-2.0.txt
+
+Copyright 2015 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package statistics
+
+import (
+	"bytes"
+	"encoding/gob"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	//"github.com/intelsdi-x/snap/core"
+	"github.com/intelsdi-x/snap/core/ctypes"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+//Random number generator
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func TestStatisticsProcessor(t *testing.T) {
+	meta := Meta()
+	Convey("Meta should return metadata for the plugin", t, func() {
+		Convey("So meta.Name should equal statistics", func() {
+			So(meta.Name, ShouldEqual, "statistics")
+		})
+		Convey("So meta.Version should equal version", func() {
+			So(meta.Version, ShouldEqual, version)
+		})
+		Convey("So meta.Type should be of type plugin.ProcessorPluginType", func() {
+			So(meta.Type, ShouldResemble, plugin.ProcessorPluginType)
+		})
+	})
+
+	proc := New()
+	Convey("Create statistics processor", t, func() {
+		Convey("So proc should not be nil", func() {
+			So(proc, ShouldNotBeNil)
+		})
+		Convey("So proc should be of type statisticsProcessor", func() {
+			So(proc, ShouldHaveSameTypeAs, &Plugin{})
+		})
+		Convey("proc.GetConfigPolicy should return a config policy", func() {
+			configPolicy, _ := proc.GetConfigPolicy()
+			Convey("So config policy should be a cpolicy.ConfigPolicy", func() {
+				So(configPolicy, ShouldHaveSameTypeAs, &cpolicy.ConfigPolicy{})
+			})
+			testConfig := make(map[string]ctypes.ConfigValue)
+			testConfig["SlidingWindowLength"] = ctypes.ConfigValueInt{Value: 23}
+			cfg, errs := configPolicy.Get([]string{""}).Process(testConfig)
+			Convey("So config policy should process testConfig and return a config", func() {
+				So(cfg, ShouldNotBeNil)
+			})
+			Convey("So testConfig processing should return no errors", func() {
+				So(errs.HasErrors(), ShouldBeFalse)
+			})
+		})
+	})
+}
+
+func TestStatisticsProcessorMetrics(t *testing.T) {
+	Convey("Statistics Processor tests", t, func() {
+		metrics := make([]plugin.PluginMetricType, 10)
+		config := make(map[string]ctypes.ConfigValue)
+
+		config["SlidingWindowLength"] = ctypes.ConfigValueInt{Value: -1}
+
+		Convey("Statistics for float64 data", func() {
+			for i := range metrics {
+				time.Sleep(3)
+				rand.Seed(time.Now().UTC().UnixNano())
+				data := randInt(23, 59)
+				//				metrics[i] = *plugin.PluginMetricType(core.JoinNamespace([]string{"foo", "bar"}), time.Now(), nil, "some unit", float64(data))
+				metrics[i] = plugin.PluginMetricType{
+					Data_:      float64(data),
+					Namespace_: []string{"foo", "bar"},
+					Timestamp_: time.Now(),
+				}
+			}
+			var buf bytes.Buffer
+			enc := gob.NewEncoder(&buf)
+			enc.Encode(metrics)
+
+			movingAverageObj := New()
+
+			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
+
+			var metricsNew []plugin.PluginMetricType
+
+			//Decodes the content into MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(receivedData))
+			dec.Decode(&metricsNew)
+			So(metrics, ShouldNotResemble, metricsNew)
+
+		})
+
+		Convey("Statistics for unknown data type", func() {
+			for i := range metrics {
+
+				data := "I am an unknow data Type"
+				// metrics[i] = *plugin.PluginMetricType([]string{"foo", "bar"}, time.Now(), nil, "some unit", data)
+				metrics[i] = plugin.PluginMetricType{
+					Data_:      data,
+					Namespace_: []string{"foo", "bar"},
+					Timestamp_: time.Now(),
+				}
+			}
+			var buf bytes.Buffer
+			enc := gob.NewEncoder(&buf)
+			enc.Encode(metrics)
+
+			movingAverageObj := New()
+
+			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
+
+			var metricsNew []plugin.PluginMetricType
+
+			//Decodes the content into MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(receivedData))
+			dec.Decode(&metricsNew)
+			So(metrics, ShouldNotResemble, metricsNew)
+
+		})
+
+	})
+}
