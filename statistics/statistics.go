cae1c8c0d1c192b0a89143ed2986acf89b727a6b
Balaji Subramaniam <balaji.warft@gmail.com>
2016-05-16 21:44:09 +0000

    Modified plugin to provide all the statistics.

--- statistics/statistics.go
+++ statistics/statistics.go
@@ -16,19 +16,21 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package movingaverage
+package statistics
 
 import (
 	"bytes"
 	"encoding/gob"
 	"errors"
-	"math"
-
 	log "github.com/Sirupsen/logrus"
+	//	"math"
+	//	"reflect"
+	// "fmt"
 
 	"github.com/intelsdi-x/snap/control/plugin"
 	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
 	"github.com/intelsdi-x/snap/core/ctypes"
+	"github.com/montanaflynn/stats"
 )
 
 const (
@@ -37,11 +39,13 @@ const (
 	pluginType = plugin.ProcessorPluginType
 )
 
-var stats = []string{"mean", "median", "stddev", "variance", "90%-ile", "95%-ile", "99%-ile", "99.99%-ile",
-	"smean", "smedian", "sstddev", "svariance", "s90%-ile", "s95%-ile", "s99%-ile", "s99.99%-ile"}
+var statsString = []string{"mean", "median", "stddev", "variance", "90%-ile", "95%-ile", "99%-ile", "99.99%-ile"}
 
 type Plugin struct {
-	slidingWinSize int
+	buffer        map[string][]float64
+	bufferMaxSize int
+	bufferCurSize int
+	bufferIndex   int
 }
 
 // Meta returns a plugin meta data
@@ -55,50 +59,86 @@ func Meta() *plugin.PluginMeta {
 }
 
 func New() *Plugin {
-	p := &Plugin{slidingWinSize: 30}
+	buffer := make(map[string][]float64)
+	p := &Plugin{buffer: buffer,
+		bufferMaxSize: 100,
+		bufferCurSize: 0,
+		bufferIndex:   0}
 	return p
 }
 
-func mean(m plugin.MetricType, logger *log.Logger) (float64, error) {
+func (p *Plugin) calculateStats(buffer []float64, logger *log.Logger) (map[string]float64, error) {
+	result := make(map[string]float64)
 
-	switch t := m.Data().(type) {
-	default:
-		logger.Printf("Unknown data received: Type %T", t)
-		return 0.0, errors.New("Unknown data received: Type")
-	case float64:
-		if len(m.Data()) == 0 {
-			return math.NaN(), errors.New("No Data")
-		}
-
-		sum, _ := m.Data().Sum()
-
-		return sum / float64(len(m.Data())), nil
-	}
-
-}
-
-func (p *Plugin) calculateStats(m plugin.MetricType, logger *log.Logger) (map[string]float64, error) {
-	result := make(map[string]float64, 16)
-
-	val, err := mean(m)
+	val, err := stats.Mean(buffer)
 	if err != nil {
 		return nil, err
 	}
 
 	result["mean"] = val
 
-	//result["median"] = percentile(m, 50)
+	val, err = stats.Median(buffer)
+	if err != nil {
+		return nil, err
+	}
 
-	//	switch v := m.Data().(type) {
-	//	default:
-	//		logger.Printf("Unknown data received: Type %T", v)
-	//		return 0.0, errors.New("Unknown data received: Type")
-	//	case float64:
-	//		result["mean"] = mean()
+	result["median"] = val
 
-	//	}
+	val, err = stats.StandardDeviation(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	result["stddev"] = val
+
+	val, err = stats.Variance(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	result["var"] = val
+
+	val, err = stats.Percentile(buffer, 99)
+	if err != nil {
+		return nil, err
+	}
+
+	result["99%-ile"] = val
 
 	return result, nil
+}
+
+func concatNameSpace(namespace []string) string {
+	completeNamespace := ""
+	for i := 0; i < len(namespace); i++ {
+		completeNamespace += namespace[i]
+
+	}
+	return completeNamespace
+
+}
+
+func (p *Plugin) insertInToBuffer(val float64, ns []string) {
+
+	if p.bufferCurSize == 0 {
+		var buff = make([]float64, p.bufferMaxSize)
+		buff[0] = val
+		p.buffer[concatNameSpace(ns)] = buff
+	} else {
+		p.buffer[concatNameSpace(ns)][p.bufferIndex] = val
+	}
+}
+
+func (p *Plugin) updateCounters() {
+	if p.bufferCurSize < p.bufferMaxSize {
+		p.bufferCurSize++
+	}
+
+	if p.bufferIndex == p.bufferMaxSize-1 {
+		p.bufferIndex = 0
+	} else {
+		p.bufferIndex++
+	}
 }
 
 func (p *Plugin) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
@@ -118,40 +158,60 @@ func (p *Plugin) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
 }
 
 func (p *Plugin) Process(contentType string, content []byte, config map[string]ctypes.ConfigValue) (string, []byte, error) {
+
 	logger := log.New()
 	logger.Println("Statistics Processor started")
 
-	var metrics []plugin.MetricType
+	var metrics []plugin.PluginMetricType
 
 	if config != nil {
 		if config["SlidingWindowLength"].(ctypes.ConfigValueInt).Value > 0 {
-			p.slidingWinSize = config["SlidingWindowLength"].(ctypes.ConfigValueInt).Value
+			p.bufferMaxSize = config["SlidingWindowLength"].(ctypes.ConfigValueInt).Value
 		} else {
-			p.slidingWinSize = 30
+			p.bufferMaxSize = 100
 		}
 	} else {
-		p.slidingWinSize = 30
+		p.bufferMaxSize = 100
 	}
 
-	//Decodes the content into MetricType
+	//Decodes the content into PluginMetricType
 	dec := gob.NewDecoder(bytes.NewBuffer(content))
 	if err := dec.Decode(&metrics); err != nil {
 		logger.Printf("Error decoding: error=%v content=%v", err, content)
 		return "", nil, err
 	}
 
-	for i, m := range metrics {
-		//Determining the type of data
+	for i, _ := range metrics {
 		logger.Printf("Data received %v", metrics[i].Data())
 
-		metrics[i].Data_, _ = p.calculateStats(m, logger)
-		logger.Printf("Statistics %v", metrics[i].Data())
+		switch v := metrics[i].Data().(type) {
+		default:
+			logger.Printf("Unknown Data Type Received: Type %T", v)
+			return "", nil, errors.New("Unknown Data Type Received.")
+		case float64:
+			p.insertInToBuffer(metrics[i].Data().(float64), metrics[i].Namespace())
 
+		}
 	}
 
+	p.updateCounters()
+
+	for i, _ := range metrics {
+		if p.bufferCurSize < p.bufferMaxSize {
+			metrics[i].Data_, _ = p.calculateStats(p.buffer[concatNameSpace(metrics[i].Namespace())][0:p.bufferCurSize], logger)
+		} else {
+			metrics[i].Data_, _ = p.calculateStats(p.buffer[concatNameSpace(metrics[i].Namespace())], logger)
+		}
+
+		logger.Printf("Statistics %v", metrics[i].Data())
+	}
+
+	gob.Register(map[string]float64{})
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
-	enc.Encode(metrics)
+	if err := enc.Encode(metrics); err != nil {
+		return "", nil, err
+	}
 
 	return contentType, buf.Bytes(), nil
 }
