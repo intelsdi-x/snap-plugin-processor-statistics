705ff9d59edb5f0ef9b1ddd75461ebd93f0075bb
marea-cobb <cobb.marea@gmail.com>
2016-09-21 14:40:29 +0000

    (SDI-2027) Updated statistics.go and statistics_test.go.

--- statistics/statistics_test.go
+++ statistics/statistics_test.go
@@ -24,6 +24,8 @@ package statistics
 import (
 	"bytes"
 	"encoding/gob"
+	"log"
+	"math"
 	"math/rand"
 	"testing"
 	"time"
@@ -83,27 +85,130 @@ func TestStatisticsProcessor(t *testing.T) {
 func TestStatisticsProcessorMetrics(t *testing.T) {
 	Convey("Statistics Processor tests", t, func() {
 		metrics := make([]plugin.MetricType, 10)
-		config := make(map[string]ctypes.ConfigValue)
+		data := [10]float64{5, 12, 7, 9, 33, 53, 24, 16, 18, 1}
 
-		config["SlidingWindowLength"] = ctypes.ConfigValueInt{Value: -1}
+		time := [10]time.Time{time.Now().Add(12 * time.Hour),
+			time.Now().Add(22 * time.Hour),
+			time.Now().Add(9 * time.Hour),
+			time.Now().Add(10 * time.Hour),
+			time.Now().Add(1 * time.Hour),
+			time.Now().Add(2 * time.Hour),
+			time.Now().Add(3 * time.Hour),
+			time.Now().Add(5 * time.Hour),
+			time.Now().Add(6 * time.Hour),
+			time.Now().Add(7 * time.Hour),
+		}
+		config := make(map[string]ctypes.ConfigValue)
+		config["SlidingWindowLength"] = ctypes.ConfigValueInt{Value: 5}
+
+		empty := []float64(nil)
 
 		Convey("Statistics for float64 data", func() {
 			for i := range metrics {
-				rand.Seed(time.Now().UTC().UnixNano())
-				data := randInt(23, 59)
 				metrics[i] = plugin.MetricType{
-					Data_:      float64(data),
+					Data_:      data[i],
 					Namespace_: core.NewNamespace("foo", "bar"),
-					Timestamp_: time.Now(),
+					Timestamp_: time[i],
 				}
 			}
+
 			var buf bytes.Buffer
 			enc := gob.NewEncoder(&buf)
 			enc.Encode(metrics)
 
-			movingAverageObj := New()
+			statisticsObj := New()
+			_, stats, err := statisticsObj.Process("snap.gob", buf.Bytes(), config)
 
-			movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var results []plugin.MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(stats))
+			err = dec.Decode(&results)
+
+			if err != nil {
+				log.Fatal("decode", err)
+			}
+
+			modes := [][]float64{[]float64{33}, empty, empty, empty, empty, empty, empty, empty, empty, empty}
+
+			expected := make(map[string][]float64)
+			expected["Count"] = []float64{1, 2, 3, 4, 5, 5, 5, 5, 5, 5}
+			expected["Mean"] = []float64{33, 43, 36.66666667, 31.5, 28.8, 22.4, 13.2, 10.2, 8, 6.8}
+			expected["Median"] = []float64{33, 43, 33, 28.5, 24, 18, 16, 9, 7, 7}
+			expected["Sum"] = []float64{33, 86, 110, 126, 144, 112, 66, 51, 40, 34}
+			expected["Standard Deviation"] = []float64{0, 10, 12.120, 13.793, 13.467, 17.072, 8.183, 6.177, 5.657, 3.709}
+			expected["Variance"] = []float64{0, 100, 146.889, 190.25, 181.36, 291.44, 66.96, 38.16, 32, 13.76}
+			expected["Maximum"] = []float64{33, 53, 53, 53, 53, 53, 24, 18, 18, 12}
+			expected["Minimum"] = []float64{33, 33, 24, 16, 16, 1, 1, 1, 1, 1}
+			expected["99%-ile"] = []float64{33, 53, 53, 53, 53, 53, 24, 18, 18, 12}
+			expected["95%-ile"] = []float64{33, 53, 53, 53, 53, 53, 24, 18, 18, 12}
+			expected["Kurtosis"] = []float64{math.NaN(), math.NaN(), math.NaN(), 1.8964, 2.337, 2.563, 1.6874, 1.6624, 2.4383, 2.0035}
+			expected["Skewness"] = []float64{math.NaN(), math.NaN(), 0.426, 0.552, 0.883, 0.744, -0.242, -0.122, 0.696, -0.195}
+			expected["Trimean"] = []float64{math.NaN(), math.NaN(), 35.75, 30, 27, 20.75, 14.25, 9.75, 7.625, 6.875}
+			expected["Range"] = []float64{0, 20, 29, 37, 37, 52, 23, 17, 17, 11}
+
+			//Tracks current location of results
+			count := 0
+			for i, m := range results {
+				//Captures the statistic being processed while ignoring the remaining portions of the namespace
+				ns := m.Namespace().Strings()[1]
+
+				//If all 15 statistics have been compared, then increase metric count
+				if i%15 == 0 && i != 0 {
+					count++
+				}
+
+				switch ns {
+				case "Count":
+					So(m.Data(), ShouldAlmostEqual, expected["Count"][count], 0.01)
+				case "Mean":
+					So(m.Data(), ShouldAlmostEqual, expected["Mean"][count], 0.01)
+				case "Median":
+					So(m.Data(), ShouldAlmostEqual, expected["Median"][count], 0.01)
+				case "Trimean":
+					if math.IsNaN(expected["Trimean"][count]) {
+						So(m.Data(), ShouldNotBeNil)
+					} else {
+
+						So(m.Data(), ShouldAlmostEqual, expected["Trimean"][count], 0.01)
+					}
+				case "Range":
+					So(m.Data(), ShouldAlmostEqual, expected["Range"][count], 0.01)
+				case "Sum":
+					So(m.Data(), ShouldAlmostEqual, expected["Sum"][count], 0.01)
+				case "Kurtosis":
+					if math.IsNaN(expected["Kurtosis"][count]) {
+						So(m.Data(), ShouldNotBeNil)
+					} else {
+						So(m.Data(), ShouldAlmostEqual, expected["Kurtosis"][count], 0.01)
+					}
+				case "Skewness":
+					if math.IsNaN(expected["Skewness"][count]) {
+						So(m.Data(), ShouldNotBeNil)
+					} else {
+						So(m.Data(), ShouldAlmostEqual, expected["Skewness"][count], 0.01)
+					}
+				case "Standard Deviation":
+					So(m.Data(), ShouldAlmostEqual, expected["Standard Deviation"][count], 0.01)
+				case "Variance":
+					So(m.Data(), ShouldAlmostEqual, expected["Variance"][count], 0.01)
+				case "Maximum":
+					So(m.Data(), ShouldAlmostEqual, expected["Maximum"][count], 0.01)
+				case "Minimum":
+					So(m.Data(), ShouldAlmostEqual, expected["Minimum"][count], 0.01)
+				case "99%-ile":
+					So(m.Data(), ShouldAlmostEqual, expected["99%-ile"][count], 0.01)
+				case "95%-ile":
+					So(m.Data(), ShouldAlmostEqual, expected["95%-ile"][count], 0.01)
+				case "Mode":
+					So(m.Data(), ShouldResemble, modes[count])
+				default:
+					log.Println("Raw metric found")
+					log.Println("Data: %v", ns)
+				}
+			}
 
 			var metricsNew []plugin.MetricType
 			So(metrics, ShouldNotResemble, metricsNew)
@@ -116,16 +221,15 @@ func TestStatisticsProcessorMetrics(t *testing.T) {
 				metrics[i] = plugin.MetricType{
 					Data_:      data,
 					Namespace_: core.NewNamespace("foo", "bar"),
-					Timestamp_: time.Now(),
+					Timestamp_: time[i],
 				}
 			}
 			var buf bytes.Buffer
 			enc := gob.NewEncoder(&buf)
 			enc.Encode(metrics)
 
-			movingAverageObj := New()
-
-			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
+			statisticObj := New()
+			_, receivedData, _ := statisticObj.Process("snap.gob", buf.Bytes(), config)
 
 			var metricsNew []plugin.MetricType
 
